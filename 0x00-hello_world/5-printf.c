45c15be712dfc5c9d63baa8be5f24abb869d7418
holatunde <onifadeolatunde1@gmail.com>
2022-09-08 10:45:24 -0700

    c-programming 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <studio.h>
-
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of 
- * art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
  *
- * return: Always 0 (Sucess)
+ * return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  * Description: print the size of various types on the computer it is compiled
  * and run on
  *
- * return: Always 0 (sucess)
+ * return: Always 0 (success)
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n",(int) sizeof(char));
+	printf("size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
