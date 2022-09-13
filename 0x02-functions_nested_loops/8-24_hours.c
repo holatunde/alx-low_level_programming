3de419fae71ff6fe4b1327518ba8626734101be1
holatunde <onifadeolatunde1@gmail.com>
2022-09-13 14:31:57 -0700

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,39 +10,25 @@
 
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
 
-	hours_max = 58;
-	hours_ones = '0';
-
-	while (hours_tens < 3)
+	while (hours <= 23)
 	{
-		if (hours_tens == '2')
+		while (minutes <= 59)
 		{
-			hours_max = '4';
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(mins_remainder + '0');
+			minutes++;
+			_putchar('\n');
 		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_ones < '6')
-			{
-				minutes_ones = '0';
-				if (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-			minutes_tens = '0';
-			hours_ones++;
-		}
-		hours_ones = '0';
-		hours_tens++;
+		hours++;
+		minutes = 0;
+	}
