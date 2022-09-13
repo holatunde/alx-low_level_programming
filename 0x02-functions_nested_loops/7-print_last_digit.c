7ce0963993289342b738a8026c9b714fd5ba0d0d
holatunde <onifadeolatunde1@gmail.com>
2022-09-13 16:50:39 -0700

    functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @r: r is an integer
+ * @n: n is an integer
  *
  * Return: integer
  */
 
-int print_last_digit(int r)
+int print_last_digit(int n)
 {
-	int last_digit = r % 10;
+	int last_digit = n % 10;
 
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
 		_putchar(last_digit + '0');
-		return (r % 10);
+		return (n % 10);
 	}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,34 +10,26 @@
 
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int num, mult, prod;
 
-	for (row = 0; row <= 9; row++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0');
+		for (mult = 0; mult <= 9; mult++)
 		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
-
-			if (column == 0)
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+			if (prod <= 9)
 			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+}
