1e549b0210e8b23cd72f95337747dfb82c08580e
holatunde <onifadeolatunde1@gmail.com>
2022-09-13 05:30:06 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int n;
+
+	for (n = 0; n < sizeof(putchar); n++)
+	{
+		_putchar(putchar[8])
+	}
+	_putchar('\n');
+	return (0);
