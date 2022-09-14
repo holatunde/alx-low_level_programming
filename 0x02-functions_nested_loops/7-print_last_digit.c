dc63844f78cd98f185c1b024cf16583498db536c
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 01:11:38 -0700

    functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,7 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
-	{
 		last_digit *= -1;
-		_putchar(last_digit + '0');
-		return (n % 10);
-	}
+	_putchar(last_digit + '0');
+	return (last digit);
 }
