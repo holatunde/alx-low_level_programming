2dabc7375d4142536df01f657b92e85e3366c0fb
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 16:29:34 -0700

    loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,22 +8,18 @@
 
 int main(void)
 {
-	long int x, y, s, z;
-
-	z = 0;
-	s = 0;
-	x = 0;
-	y = 1;
+	unsigned long x = 0, y = 1, s = 0, z = 0;
 
 	while (s < 4000000)
 	{
 		s = x + y;
-		if (s % 2 == 0)
-			z += s;
 		x = y;
-		y = x;
+		y = s;
+
+		if (x % 2 == 0)
+			z += x;
 	}
-	printf("%li\n", z);
+	printf("%lu\n", z);
 
 	return (0);
 }
