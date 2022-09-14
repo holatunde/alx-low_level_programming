0b8e67188d84080c048b21109f41fbba72872fb8
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 12:49:55 -0700

    loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,2 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - prints the first 98 fibonacci sequence starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, x = 1, y = 2;
+	unsigned long s;
+
+	printf("%d, ", x);
+	printf("%d", y);
+	for (i = 1; i < 97; i++)
+	{
+		s = x + y;
+		putchar(',');
+		putchar(' ');
+		printf("%lu", s);
+		x = y;
+		y = s;
+	}
+	putchar('\n');
+
+	return (0);
+}
