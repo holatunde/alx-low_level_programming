e17bda5a67e25dc6e9d839ccd61d7a0e9a92fc6d
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 05:36:03 -0700

    loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,12 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-		{
 			printf("%d, ", n--);
-			printf("%d", n);
-		}
+		printf("%d", n);
 	}
-	else if (n < 98)
+	else
 	{
 		while (n < 98)
-		{
 			printf("%d, ", n++);
-			printf("%d", n);
-		}
+		printf("%d", n);
 	}
