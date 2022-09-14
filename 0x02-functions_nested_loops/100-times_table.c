07bf8bca1de1db51a7056ab5314fb627484c1ae0
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 08:04:45 -0700

    loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n: integer for which the times table will be printed
+ *
+ * Description: prints the times table
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int n, j, k;
+
+	for (n = 0; n <= 15; n++)
+	{
+		_putchar('0');
+
+		for (j = 0; j <= 15; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = n * j;
+			if (prod <= 15)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + '0');
+
+			_putchar((k % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
