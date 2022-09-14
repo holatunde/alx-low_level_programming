224db711eadc5ec624b342dba8ebca6e1351697d
holatunde <onifadeolatunde1@gmail.com>
2022-09-14 09:07:53 -0700

    loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,8 +24,12 @@ void print_times_table(int n)
 				_putchar(' ');
 
 				k = i * j;
-				if (k <= 15)
+				if (k <= 9)
 					_putchar(' ');
+				else if (k >=10 && k <= 100)
+					_putchar(' ');
+					_putchar((k / 10) % 10 + '0');
+					_putchar(k % 10 + '0');
 				else
 					_putchar((k / 10) + '0');
 
