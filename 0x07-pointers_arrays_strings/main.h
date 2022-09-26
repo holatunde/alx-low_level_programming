875d2212c96df3002e91c87d69192e8a30e837fd
holatunde <onifadeolatunde1@gmail.com>
2022-09-26 13:10:03 -0700

    crackme

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * void prototypea()
  */
 
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -15,5 +16,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
