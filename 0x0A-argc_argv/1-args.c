e010bc35969e6f48583f3aa91e459b1ff44f9b4f
holatunde <onifadeolatunde1@gmail.com>
2022-09-30 10:54:08 -0700

    argument to main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,7 +10,7 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[] __attributes__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
