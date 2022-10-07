12ef32dcbcbe835916ddee84f10a3af4d69c8d82
holatunde <onifadeolatunde1@gmail.com>
2022-10-07 14:43:22 -0700

    more malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,6 +17,5 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
-int main(void);
 
 #endif /* MAIN_H */
