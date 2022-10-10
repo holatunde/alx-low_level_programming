55cc4e8411ffce8f10d4b79aef6cd36f18c9921f
holatunde <onifadeolatunde1@gmail.com>
2022-10-10 08:24:04 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a function
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * description: a function
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
